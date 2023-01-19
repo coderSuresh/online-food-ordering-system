6469e820ab943172e8715d93414b7912850fec41
codersuresh <sd807141@gmail.com>
2023-01-19 14:49:54 +0545

    fix: path issue

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ const google_auth = getAuth(app);
 const googel_provider = new GoogleAuthProvider(app);
 
 const google = document.querySelector(".google")
-google.addEventListener('click', (e) => {
+google && google.addEventListener('click', (e) => {
 
     signInWithPopup(google_auth, googel_provider)
         .then((result) => {
@@ -85,16 +85,6 @@ google.addEventListener('click', (e) => {
             const auth = getAuth();
             onAuthStateChanged(auth, (user) => {
                 if (user !== null) {
-<<<<<<< HEAD
-                    user.providerData.forEach((profile) => {
-                        console.log("Sign-in provider: " + profile.providerId);
-                        console.log("  Provider-specific UID: " + profile.uid);
-                        console.log("  Name: " + profile.displayName);
-                        console.log("  Email: " + profile.email);
-                        console.log("  Photo URL: " + profile.photoURL);
-                    });
-                }
-=======
                     if (document.cookie = "user=" + user) {
                         user.providerData.forEach((profile) => {
                             let sign_in_provider = profile.providerId;
@@ -112,7 +102,6 @@ google.addEventListener('click', (e) => {
                     }
                 }
 
->>>>>>> a834fe89e1f58833733895d5e5eb645b68a3f8fe
             });
             // ...
         }).catch((error) => {
@@ -148,35 +137,31 @@ facebook && facebook.addEventListener('click', (e) => {
             onAuthStateChanged(auth, (user) => {
                 if (user !== null) {
                     user.providerData.forEach((profile) => {
-                        console.log("Sign-in provider: " + profile.providerId);
-                        console.log("  Provider-specific UID: " + profile.uid);
-                        console.log("  Name: " + profile.displayName);
-                        console.log("  Email: " + profile.email);
+                        let sign_in_provider = profile.providerId;
+                        document.cookie = "sign_in_provider" + sign_in_provider;
+                        let profile_name = profile.displayName;
+                        document.cookie = "profile_name" + profile_name;
+                        let email = profile.email;
+                        document.cookie = "email" + email;
                         console.log("  Photo URL: " + profile.photoURL);
+
                     });
                 }
             });
-            let sign_in_provider = profile.providerId;
-            document.cookie = "sign_in_provider" + sign_in_provider;
-            let profile_name = profile.displayName;
-            document.cookie = "profile_name" + profile_name;
-            let email = profile.email;
-            document.cookie = "email" + email;
-            console.log("  Photo URL: " + profile.photoURL);
-
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = FacebookAuthProvider.credentialFromError(error);
+            alert(errorMessage);
+            // ...
         });
-})
-    .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = FacebookAuthProvider.credentialFromError(error);
-        alert(errorMessage);
-        // ...
-    });
+});
+
 // handle menu categories (front end validation)
 // TODO: replace this with client side validation
 const categories = document.querySelectorAll(".food_category")
