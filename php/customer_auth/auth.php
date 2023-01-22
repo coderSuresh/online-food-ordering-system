bd28c8ffefe66a122ffa2d3e13d27f223812ee5f
Mukesh Mahato <mukeshmahato843@gmail.com>
2023-01-22 15:00:23 +0545

    fix: stacked error message in login

--- php/customer_auth/auth.php
+++ php/customer_auth/auth.php
@@ -3,23 +3,25 @@ session_start();
 include('../../config.php');
 
 if (isset($_POST['login'])) {
-      $username = mysqli_real_escape_string($conn, $_POST['username']);
-      $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
-      $sql = "SELECT username,password from customer";
-      $result = mysqli_query($conn, $sql);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
+    $sql = "SELECT username,password from customer where username='$username'";
+    $result = mysqli_query($conn, $sql);
 
-      while ($data = mysqli_fetch_array($result)) {
-            if ($username != $data['username']) {
-                  $_SESSION['username'] = "Invalid username";
-                  header("Location:./login.php");
-            } else if ($password != $data['password']) {
-                  $_SESSION['pass'] = "Invalid password";
-                  header("Location:./login.php");
-            } else {
-                  $_SESSION['success'] = "Login success";
+    if (mysqli_num_rows($result) > 0) {
+        while ($data = mysqli_fetch_array($result)) {
+            if ($password != $data['password']) {
+                $_SESSION['pass'] = "Invalid password";
+                header("Location:./login.php");
+            }else{
+                $_SESSION['success'] = "Login success";
                   header("Location:../../index.php");
             }
-      }
+        }
+    } else {
+        $_SESSION['username'] = "Invalid username";
+        header("Location:./login.php");
+    }
 } else {
-      header("Location:Location:./login.php");
+    header("Location:Location:./login.php");
 }
