27ca18199ba25b0bf2ceb8994eec183e48b6ba73
Paras Bhandari <50918320+official-paras-bhandari@users.noreply.github.com>
2023-02-07 19:12:55 +0545

    icon change

--- admin/users/components/sidebar.php
+++ admin/users/components/sidebar.php
@@ -32,7 +32,7 @@
              <a href="#">
                  <article class="dashboard_sidebar_content border-curve-lg flex items-center justify-center">
                      <div class="flex items-center justify-start">
-                         <img class="dashboard_sidebar_content_img" src="../../images/ic_view.svg" alt="view" aria-hidden="true">
+                         <img class="dashboard_sidebar_content_img" src="../../images/ic_manage_food.svg" alt="manage food" aria-hidden="true">
                          <h4 class="dashboard_sidebar_content_text hide-on-close">Food Items</h4>
                      </div>
                      <img src="../../images/ic_accordion_arrow.svg" class="accordion_arrow hide-on-close" aria-hidden="true" alt="accordion arrow">
