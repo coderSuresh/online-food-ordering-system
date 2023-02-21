b416514b112ca6ad476d17a336dd44852810cde6
codersuresh <sd807141@gmail.com>
2023-02-21 21:07:38 +0545

    feat: add reject option for prepared and delivering foo

--- admin/order-details.php
+++ admin/order-details.php
@@ -372,6 +372,19 @@
                                                     </div>
                                                 </button>
                                             </form>
+                                            <form action="./backend/order/reject.php" method="post" class="flex reject_form mt-20 items-center justify-start">
+                                                <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+                                                <input type="hidden" name="aos_id" value="<?php echo $row["aos_id"]; ?>">
+                                                <input type="hidden" class="hidden-reject_reason" name="reject-reason" value="">
+                                            </form>
+                                            <div class="flex items-center justify-start">
+                                                <button type="submit" name="reject" class="no_bg no_outline reject_btn">
+                                                    <div class="flex items-center justify-start">
+                                                        <img src="../images/ic_reject.svg" alt="reject icon">
+                                                        <p class="body-text">Reject</p>
+                                                    </div>
+                                                </button>
+                                            </div>
                                         <?php
                                         } else if ($status == "delivering") {
                                         ?>
@@ -385,6 +398,19 @@
                                                     </div>
                                                 </button>
                                             </form>
+                                            <form action="./backend/order/reject.php" method="post" class="flex reject_form mt-20 items-center justify-start">
+                                                <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+                                                <input type="hidden" name="aos_id" value="<?php echo $row["aos_id"]; ?>">
+                                                <input type="hidden" class="hidden-reject_reason" name="reject-reason" value="">
+                                            </form>
+                                            <div class="flex items-center justify-start">
+                                                <button type="submit" name="reject" class="no_bg no_outline reject_btn">
+                                                    <div class="flex items-center justify-start">
+                                                        <img src="../images/ic_reject.svg" alt="reject icon">
+                                                        <p class="body-text">Reject</p>
+                                                    </div>
+                                                </button>
+                                            </div>
                                         <?php
                                         }
                                         ?>
