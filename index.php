3c056e209bf82d52dee03fe7722466edd56d51a0
codersuresh <sd807141@gmail.com>
2023-02-28 15:13:54 +0545

    feat: add best selling row in home page

--- index.php
+++ index.php
@@ -23,15 +23,14 @@
             <a href="./details.php?name=meat lovers pizza"><img src="./images/restrohub offer banner.png" class="offer_img" alt="offer banner"></a>
         </section>
 
-        <h2 class="heading our_special ml-auto mt-20">Our Special
+        <h2 class="heading our_special ml-auto mt-60">Our Special
             <hr class="underline ml-auto no_outline">
         </h2>
 
         <!-- cards container -->
         <div class="food_cards flex gap wrap justify-center">
-
             <?php
-            include('./config.php');
+            require("./config.php");
             $sql = "SELECT * FROM food where special = 1 and disabled = 0";
             $result = mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) > 0) {
@@ -68,6 +67,52 @@
             ?>
         </div>
         <!-- card end -->
+
+        <h2 class="heading our_special ml-auto mt-60">Best Selling
+            <hr class="underline ml-auto no_outline">
+        </h2>
+
+        <div class="food_cards flex gap wrap justify-center">
+            <?php
+            $sql_fetch_food = "select (count(f_id) * qty), f_id from orders inner join aos on orders.id = aos.order_id where aos.status = 'delivered' group by f_id order by (count(f_id) * qty) desc limit 4";
+            $res_fetch_food = mysqli_query($conn, $sql_fetch_food);
+            $count_fetch_food = mysqli_num_rows($res_fetch_food);
+            while ($top_data = mysqli_fetch_assoc($res_fetch_food)) {
+                $f_id = $top_data['f_id'];
+                $sql_food = "SELECT * FROM food where f_id = $f_id";
+                $res_food = mysqli_query($conn, $sql_food);
+                $data = mysqli_fetch_assoc($res_food);
+            ?>
+                <div class="menu_food-card border-curve shadow">
+                    <!-- testing badge or something for card -->
+                    <p class="card__tag text-center heading"><?php if ($data['veg'] == 1)
+                                                                    echo "Veg";
+                                                                else
+                                                                    echo "Non-veg"; ?></p>
+
+                    <div class="card__food-img">
+                        <img src="./uploads/foods/<?php echo $data['img']; ?>" class="food_img w-full" alt="food item">
+                    </div>
+                    <article class="card__food-info flex items-center">
+                        <h2 class="card__food-title heading"><?php echo $data['name']; ?></h2>
+                        <p class="card__food-price heading">Rs. <?php echo $data['price']; ?></p>
+                    </article>
+                    <p class="card__food-desc"><?php echo $data['short_desc']; ?></p>
+                    <div class="card__btns flex">
+                        <div class="form mr-10">
+                            <a href="./details.php?name=<?php echo $data['name']; ?>" class="button card__btn flex justify-center border-curve" name="view"><img src="./images/ic_eye.svg" alt="view"></a>
+                        </div>
+                        <form action="#" method="post" class="form_food-card" name="form_food-card">
+                            <input type="hidden" name="f_id" value="<?php echo $data['f_id']; ?>">
+                            <button href="submit" class="button card__btn btn_add-to-cart flex justify-center border-curve" name="add-to-card"><img src="./images/ic_add-cart.svg" alt="add to cart"></button>
+                        </form>
+                    </div>
+                </div>
+            <?php
+            }
+
+            ?>
+        </div>
 
         <div class="button w-fit border-curve mt-20 ml-auto">
             <a href="./menu.php">View All Foods</a>
