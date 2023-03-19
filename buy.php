e0da987e028687d9f1c8ea58c134295da966d844
codersuresh <sd807141@gmail.com>
2023-03-19 17:57:23 +0545

    refactor: reuse header

--- buy.php
+++ buy.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['success'])) {
-    echo '<script>alert("Please login first to access this page")</script>';
-    header("Location: ./customer_auth/login.php");
-}
-require('./config.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,39 +13,14 @@ require('./config.php');
 </head>
 
 <body>
-    <header>
-        <nav class="top_nav flex items-center">
-            <div class="logo__back-btn flex items-center">
-                <!-- back btn -->
-                <button class="nav__btn-back no_bg no_outline"><img src="./images/ic_back.svg" alt="go back"></button>
-                <a href="./index.php" class="logo heading flex items-center"><img src="./images/logo.png" alt="logo">Restro
-                    <span>Hub</span>
-                </a>
-            </div>
-
-            <ul class="flex items-center">
-                <li class="flex direction-col"><a href="menu.php">Menu</a></li>
-
-                <!-- nav search form -->
-                <?php require './components/search-form.php'; ?>
-
-                <!-- show profile icon if the user is logged in -->
-                <?php
-                if (isset($_SESSION['success'])) {
-                    echo '<li class="flex direction-col">
-                            <img src="./images/logo.png" class="user_profile_icon" alt="account">
-                            <div class="logout-dropdown border-curve shadow p-20">
-                                <a href="./customer_auth/logout.php">Logout</a>   
-                            </div>                         
-                          </li>
-                          ';
-                } else {
-                    echo '<li class="flex direction-col"><a href="./customer_auth/login.php"><img src="./images/ic_acc.svg" alt="account">
-                    <span class="nav__tooltip shadow p-20">Login</span></a>';
-                }
-                ?>
-        </nav>
-    </header>
+    <?php
+    require './components/header.php';
+    if (!isset($_SESSION['success'])) {
+        echo '<script>alert("Please login first to access this page")</script>';
+        header("Location: ./customer_auth/login.php");
+    }
+    require('./config.php');
+    ?>
 
     <main class="checkout_page" style="margin: 0 40px 40px;">
         <button class="go_top no_bg no_outline"><img src="./images/ic_top.svg" alt="go to top"></button>
--- checkout.php
+++ checkout.php
@@ -1,9 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['success'])) {
-    header('Location: ./index.php');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,41 +14,12 @@ if (!isset($_SESSION['success'])) {
 </head>
 
 <body>
-    <header>
-        <nav class="top_nav flex items-center">
-            <div class="logo__back-btn flex items-center">
-                <!-- back btn -->
-                <button class="nav__btn-back no_bg no_outline"><img src="./images/ic_back.svg" alt="go back"></button>
-                <a href="./index.php" class="logo heading flex items-center"><img src="./images/logo.png" alt="logo">Restro
-                    <span>Hub</span>
-                </a>
-            </div>
-
-            <ul class="flex items-center">
-                <li class="flex direction-col"><a href="menu.php">Menu</a></li>
-
-                <!-- nav search form -->
-                <?php require './components/search-form.php'; ?>
-
-                <!-- show profile icon if the user is logged in -->
-                <?php
-                if (isset($_SESSION['success'])) {
-                    echo '<li class="flex direction-col">
-                            <img src="./images/logo.png" class="user_profile_icon" alt="account">
-                            <div class="logout-dropdown border-curve shadow p-20">
-                                <a href="./customer_auth/logout.php">Logout</a>   
-                            </div>                         
-                          </li>
-                          ';
-                } else {
-                    echo '<li class="flex direction-col"><a href="./customer_auth/login.php"><img src="./images/ic_acc.svg" alt="account">
-                    <span class="nav__tooltip shadow p-20">Login</span></a>';
-                }
-                ?>
-                <div class="count-top cart_count-top shadow" style="position:fixed; right: -200px;"></div>
-            </ul>
-        </nav>
-    </header>
+    <?php
+    require './components/header.php';
+    if (!isset($_SESSION['success'])) {
+        header('Location: ./index.php');
+    }
+    ?>
 
     <main class="checkout_page" style="margin: 0 40px 40px;">
 
