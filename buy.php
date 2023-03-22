fd9930e64e9a5845bb18f61fc95a965beb454d54
codersuresh <sd807141@gmail.com>
2023-03-22 12:15:00 +0545

    feat: reset time if future date is selected

--- buy.php
+++ buy.php
@@ -167,27 +167,7 @@
                     </div>
 
                     <div class="for_later_inputs">
-                        <label for="date">Date:*</label>
-                        <input type="date" name="date" min="<?php echo $date; ?>" max="<?php echo date_format((date_add(date_create($date), date_interval_create_from_date_string("3 days"))), 'Y-m-d'); ?>" class="p_7-20" id="date" required>
-                        <label for="time">Time:*</label>
-
-                        <?php
-                        if (date('H:i', strtotime($time . ' +30 minutes')) > $end_time || date('H:i', strtotime($time . ' +30 minutes')) < $start_time) {
-                            echo "<p><b>Sorry, we are not open at this time. Please select another date if you wish to order for later.</b></p>";
-                        }
-
-                        ?>
-
-                        <select name="time" required>
-                            <option value=''>__SELECT__</option>
-                            <?php
-                            while (date('H:i', strtotime($time . ' +30 minutes')) <= $end_time) {
-                                $time = date('H:i', strtotime($time . ' +30 minutes'))
-                            ?> <option value='<?php echo $time; ?>'> <?php echo $time; ?> </option>
-                            <?php
-                            }
-                            ?>
-                        </select>
+                        <!-- coming from js below -->
                     </div>
 
                     <label for="note"> Note: </label>
@@ -240,9 +220,62 @@
 
         forLaterCheckBox.addEventListener("click", () => {
             if (forLaterCheckBox.checked) {
-                forLaterInputs.classList.add("visi")
+                forLaterInputs.innerHTML = `
+                        <label for="date">Date:*</label>
+                        <input type="date" name="date" 
+                                min="<?php echo $date; ?>" 
+                                max="<?php echo date_format((date_add(date_create($date), date_interval_create_from_date_string("3 days"))), 'Y-m-d'); ?>" 
+                                class="p_7-20" id="date" 
+                                required>
+                       
+                        <label for="time">Time:*</label>
+                        <?php
+                        if (date('H:i', strtotime($time . ' +30 minutes')) > $end_time || date('H:i', strtotime($time . ' +30 minutes')) < $start_time) {
+                            echo "<p><b>Sorry, we are not open at this time. Please select another date if you wish to order for later.</b></p>";
+                        }
+                        ?>
+
+                        <select name="time" required>
+                            <option value=''>__SELECT__</option>
+                            <optgroup class="time_option"> <?php
+                                                            while (date('H:i', strtotime($time . ' +30 minutes')) <= $end_time) {
+                                                                $time = date('H:i', strtotime($time . ' +30 minutes'))
+                                                            ?> <option value='<?php echo $time; ?>'> <?php echo $time; ?> </option>
+                        <?php
+                                                            }
+                        ?> </optgroup>
+                        </select>
+                        `
+
+
+                // ============== listen for date change and set time options accordingly ==============
+
+                console.log("hello")
+
+                const dateInput = document.querySelector('#date')
+                const timeOption = document.querySelector('.time_option')
+
+                dateInput.addEventListener("change", () => {
+
+                    const date = dateInput.value
+                    const today = '<?php echo $date; ?>'
+
+                    if (date != today) {
+                        console.log("not today")
+                        timeOption.innerHTML = `
+                    <?php
+                    while (date('H:i', strtotime($start_time . ' +30 minutes')) <= $end_time) {
+                        $start_time = date('H:i', strtotime($start_time . ' +30 minutes'))
+                    ?> <option value='<?php echo $start_time; ?>'> <?php echo $start_time; ?> </option>
+                    <?php
+                    }
+                    ?>`
+                    } else {
+                        console.log("today")
+                    }
+                })
             } else {
-                forLaterInputs.classList.remove("class", "visi")
+                forLaterInputs.innerHTML = ''
             }
         })
     </script>
