0874de5c7642e9631dab7e9e5b8dee48aaa104a3
codersuresh <sd807141@gmail.com>
2023-03-23 21:31:44 +0545

    fix: auth issue

--- customer_auth/auth.php
+++ customer_auth/auth.php
@@ -6,14 +6,14 @@ if (isset($_POST['login'])) {
     $sql_username = "SELECT active FROM customer WHERE username = '$username'";
     $result = mysqli_query($conn, $sql_username);
     $row = mysqli_fetch_assoc($result);
-    if ($row['active'] == 1) {
-        $sql_status = "Select status,username from customer where username";
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
-        $sql = "SELECT id,username,password,status from customer where username='$username'";
-        $result = mysqli_query($conn, $sql);
+    $sql_status = "Select status,username from customer where username";
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
+    $sql = "SELECT id,username,password,status from customer where username='$username'";
+    $result = mysqli_query($conn, $sql);
 
-        if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
+        if ($row['active'] == 1) {
             while ($data = mysqli_fetch_array($result)) {
                 if ($data['status'] == "verified") {
                     if ($password != $data['password']) {
@@ -31,11 +31,11 @@ if (isset($_POST['login'])) {
                 }
             }
         } else {
-            $_SESSION['username'] = "Incorrect username";
-            header("Location:./login.php");
+            $_SESSION["block"] = "You account has been blocked";
         }
     } else {
-        $_SESSION["block"] = "You account has been blocked";
+        $_SESSION['username'] = "Incorrect username";
+        header("Location:./login.php");
     }
 } else {
     header("Location:Location:./login.php");
