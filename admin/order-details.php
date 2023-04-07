327b5c30cdc0ab2db684114b2fdad6ae83a835da
codersuresh <sd807141@gmail.com>
2023-04-07 20:37:23 +0545

    remove useless shit

--- admin/order-details.php
+++ admin/order-details.php
@@ -24,58 +24,6 @@
 
         <section class="dashboard_inner-head flex items-center">
             <h2>Current Order Details</h2>
-            <img src="../images/ic_calender.svg" class="filter_by_date popper-btn" alt="filter">
-        </section>
-
-        <section class="modal items-center justify-center">
-            <div class="modal_form-container p-20 small-modal shadow border-curve-md">
-
-                <div class="modal_title-container flex items-center">
-                    <h2 class="modal-title">Select an option</h2>
-                    <button class="close-icon no_bg no_outline"><img src="../images/ic_cross.svg" alt="close"></button>
-                </div>
-
-                <form action="#" method="post" class="date_filter_modal_form">
-
-                    <div class="date_filter_form_options flex justify-start items-center wrap">
-                        <div class="flex justify-start items-center">
-                            <input type="radio" name="filter_option" id="filter_option-today" checked>
-                            <label for="filter_option-today"> &nbsp; Today</label>
-                        </div>
-                        <div class="flex justify-start items-center">
-                            <input type="radio" name="filter_option" id="filter_option-yesterday">
-                            <label for="filter_option-yesterday"> &nbsp; Yesterday</label>
-                        </div>
-                        <div class="flex justify-start items-center">
-                            <input type="radio" name="filter_option" id="filter_option-last-week">
-                            <label for="filter_option-last-week"> &nbsp; Last week</label>
-                        </div>
-                        <div class="flex justify-start items-center">
-                            <input type="radio" name="filter_option" id="filter_option-last-month">
-                            <label for="filter_option-last-month"> &nbsp; Last month</label>
-                        </div>
-                        <div class="flex justify-start items-center">
-                            <input type="radio" name="filter_option" id="filter_option-custom">
-                            <label for="filter_option-custom"> &nbsp; Custom</label>
-                        </div>
-
-                    </div>
-
-                    <!-- filter using custom date range start -->
-                    <div class="flex justify-evenly date_filter_form_option-custom">
-                        <div class="flex direction-col">
-                            <label for="start-date">From:</label>
-                            <input type="date" name="start-date" id="start-date">
-                        </div>
-                        <div class="flex direction-col">
-                            <label for="end-date">To:</label>
-                            <input type="date" name="end-date" id="end-date">
-                        </div>
-                    </div>
-                    <!-- filter using custom date range end -->
-                    <button type="submit" class="no_outline border-curve-md w-full button mt-20">Filter</button>
-                </form>
-            </div>
         </section>
 
         <div class="flex items-center mt-20">
