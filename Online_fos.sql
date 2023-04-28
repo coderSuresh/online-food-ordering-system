62d083ff24cb644099662e568da744530f6b4568
codersuresh <sd807141@gmail.com>
2023-04-28 09:28:23 +0545

    fix: db update

--- Online_fos.sql
+++ Online_fos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 06, 2023 at 03:50 PM
+-- Generation Time: Apr 28, 2023 at 05:42 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -60,12 +60,22 @@ CREATE TABLE `aos` (
 --
 
 INSERT INTO `aos` (`aos_id`, `order_id`, `status`, `date`, `delivered_at`) VALUES
-(376, 316, 'delivered', '2023-04-04 12:35:31', '2023-04-04 12:39:22'),
-(377, 317, 'delivered', '2023-04-04 12:48:03', '2023-04-10 14:40:03'),
-(378, 318, 'delivered', '2023-04-04 12:51:42', '2023-02-04 15:52:18'),
-(379, 319, 'delivered', '2023-04-04 13:11:02', '2023-04-05 13:13:25'),
-(380, 320, 'delivered', '2023-04-04 13:11:02', '2023-04-06 13:13:25'),
-(381, 321, 'delivered', '2023-04-04 16:15:04', '2023-04-04 16:15:54');
+(382, 322, 'delivered', '2023-04-08 10:00:02', '2023-04-08 10:01:43'),
+(383, 323, 'delivered', '2023-04-08 12:44:55', '0000-00-00 00:00:00'),
+(384, 324, 'delivered', '2023-04-10 09:28:56', '2023-04-10 09:29:57'),
+(385, 325, 'pending', '2023-04-10 09:51:58', '0000-00-00 00:00:00'),
+(386, 326, 'pending', '2023-04-10 17:57:00', '0000-00-00 00:00:00'),
+(387, 327, 'rejected', '2023-04-26 19:51:59', '0000-00-00 00:00:00'),
+(388, 328, 'rejected', '2023-04-26 19:51:59', '0000-00-00 00:00:00'),
+(389, 329, 'rejected', '2023-04-26 19:51:59', '0000-00-00 00:00:00'),
+(390, 330, 'rejected', '2023-04-26 17:46:35', '0000-00-00 00:00:00'),
+(391, 331, 'rejected', '2023-04-26 09:49:42', '0000-00-00 00:00:00'),
+(392, 332, 'rejected', '2023-04-26 09:49:42', '0000-00-00 00:00:00'),
+(393, 333, 'rejected', '2023-04-26 09:49:42', '0000-00-00 00:00:00'),
+(394, 334, 'rejected', '2023-04-26 09:49:42', '0000-00-00 00:00:00'),
+(395, 335, 'delivered', '2023-04-26 11:47:58', '2023-04-26 12:03:24'),
+(396, 336, 'delivered', '2023-04-26 11:47:58', '2023-04-26 12:03:24'),
+(397, 337, 'delivered', '2023-04-26 11:47:58', '2023-04-26 12:03:24');
 
 -- --------------------------------------------------------
 
@@ -91,7 +101,9 @@ INSERT INTO `cart` (`id`, `customer_id`, `food_id`, `quantity`) VALUES
 (173, 46, 31, 2),
 (174, 46, 39, 1),
 (175, 46, 40, 1),
-(183, 46, 44, 1);
+(183, 46, 44, 1),
+(251, 43, 40, 1),
+(265, 47, 43, 2);
 
 -- --------------------------------------------------------
 
@@ -139,17 +151,17 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `names`, `username`, `email`, `password`, `provider`, `date`, `status`, `active`, `otp`, `count`) VALUES
-(10, 'milan', 'milan45', 'milan45@gmail.com', '6a2801261a59fb0f75ca9c2e3560fbe3', '', '0000-00-00 00:00:00', 'not verified', 1, '225001', 0),
-(34, 'parash bhandari', 'parash', 'parashbhandari85@gmail.com', '4297f44b13955235245b2497399d7a93', '', '0000-00-00 00:00:00', 'not verified', 1, '225001', 0),
-(37, 'dklfj', 'skjfl', 'jfls@dkfj.adsfj', '4297f44b13955235245b2497399d7a93', '', '0000-00-00 00:00:00', 'not verified', 0, '225001', 0),
-(38, 'Rame', 'rame', 'rame@chor.ho', '78dacb5f238ca3ac3694f7c2114fa69c', '', '0000-00-00 00:00:00', 'not verified', 0, '225001', 0),
-(40, 'Ashish Acharya', 'ashish', 'acashish458@gmail.com', '332b3091416bc4687821c4653f1c6eb1', '', '0000-00-00 00:00:00', 'verified', 1, '225001', 0),
-(42, 'suresh dahal', 'suresh', 'sd807141@gmail.com', '3e6a0966890c85a8ca932302ad6a2405', 'email', '2023-02-18 15:34:54', 'verified', 0, '0', 0),
-(43, 'Suresh Dahal', NULL, 'itsureshdahal@gmail.com', NULL, 'google.com', '2023-02-19 13:45:31', 'verified', 1, '225001', 0),
-(44, 'suresh dahal', 'sureshdahal', 'carepc49@gmail.com', '332b3091416bc4687821c4653f1c6eb1', 'email', '2023-02-26 14:27:07', 'verified', 1, '225001', 0),
-(45, 'Bibek Mahat', 'mahat', 'mahat6981@gmail.com', 'de58bc546778fe4bae8ede6a094bca0c', 'email', '2023-03-12 15:32:24', 'verified', 1, '225001', 0),
-(46, 'Parash', 'Hero01', 'shaktishiv209@gmail.com', '33ec856129f7ec4de61221a67917d334', 'email', '2023-03-14 18:51:53', 'verified', 1, '225001', 0),
-(47, 'suresh dahal', NULL, 'dahal4524@gmail.com', NULL, 'google.com', '2023-04-04 16:36:00', 'verified', 1, NULL, 0);
+(10, 'milan', 'milan45', 'milan45@gmail.com', '6a2801261a59fb0f75ca9c2e3560fbe3', '', '0000-00-00 00:00:00', 'not verified', 1, '253583', 0),
+(34, 'parash bhandari', 'parash', 'parashbhandari85@gmail.com', '4297f44b13955235245b2497399d7a93', '', '0000-00-00 00:00:00', 'not verified', 1, '253583', 0),
+(37, 'dklfj', 'skjfl', 'jfls@dkfj.adsfj', '4297f44b13955235245b2497399d7a93', '', '0000-00-00 00:00:00', 'not verified', 0, '253583', 0),
+(38, 'Rame', 'rame', 'rame@chor.ho', '78dacb5f238ca3ac3694f7c2114fa69c', '', '0000-00-00 00:00:00', 'not verified', 0, '253583', 0),
+(40, 'Ashish Acharya', 'ashish', 'acashish458@gmail.com', '332b3091416bc4687821c4653f1c6eb1', '', '0000-00-00 00:00:00', 'verified', 1, '253583', 0),
+(43, 'Suresh Dahal', NULL, 'itsureshdahal@gmail.com', NULL, 'google.com', '2023-02-19 13:45:31', 'verified', 1, '253583', 0),
+(44, 'suresh dahal', 'sureshdahal', 'carepc49@gmail.com', '332b3091416bc4687821c4653f1c6eb1', 'email', '2023-02-26 14:27:07', 'verified', 1, '253583', 0),
+(45, 'Bibek Mahat', 'mahat', 'mahat6981@gmail.com', 'de58bc546778fe4bae8ede6a094bca0c', 'email', '2023-03-12 15:32:24', 'verified', 1, '253583', 0),
+(46, 'Parash', 'Hero01', 'shaktishiv209@gmail.com', '33ec856129f7ec4de61221a67917d334', 'email', '2023-03-14 18:51:53', 'verified', 1, '253583', 0),
+(47, 'suresh dahal', NULL, 'dahal4524@gmail.com', NULL, 'google.com', '2023-04-04 16:36:00', 'verified', 1, '253583', 0),
+(48, 'Coder Suresh', 'codersuresh', 'sd807141@gmail.com', '3495a890e500304ce83fc28b928c5269', 'email', '2023-04-08 13:22:47', 'verified', 1, '0', 0);
 
 -- --------------------------------------------------------
 
@@ -231,7 +243,7 @@ INSERT INTO `food` (`f_id`, `img`, `name`, `price`, `cooking_time`, `description
 (36, 'veg fried momo.jpg', 'Veg Fried Momo', 120, 25, 'For those who love crispy, fried food, these veggie dumplings are the perfect choice. The savory and spicy filling is encased in a crispy, golden-brown exterior that\'s sure to satisfy your cravings for something crunchy and delicious.', 1, 'vfm', 'Crispy, fried veggie dumplings', 'flour and water\r\nveggies (such as cabbage, carrots, and onions)\r\nspices (such as ginger, garlic, and cumin).', 0, 0, 114),
 (37, 'momo.jpg', 'Buff Momo', 110, 20, 'Made with a savory buff filling and a tender, doughy exterior, these steamed buff dumplings are a popular snack in many Asian countries. The rich and flavorful buff filling is the perfect complement to the soft and chewy dough, making these dumplings a delicious and satisfying snack.', 0, 'bm', 'Savory buff filling in a soft shell.', 'flour and water\r\nBuff keema\r\nveggies (such as cabbage, carrots, and onions)\r\nspices (such as ginger, garlic, and cumin).                ', 0, 0, 114),
 (38, 'buff c momo.jpg', 'Buff C Momo', 160, 25, 'For a rich and creamy twist on classic buff dumplings, try these buff and cheese dumplings. The cheesy filling adds a luxurious and creamy flavor to the already savory buff filling, making these dumplings a popular choice for those who love the taste of cheese.', 0, 'bcm', 'Rich buff and cheese filling.', 'flour and water\r\nBuff keema\r\nveggies (such as cabbage, carrots, and onions)\r\nspices (such as ginger, garlic, and cumin).                                                                                         ', 0, 0, 114),
-(39, 'buff fried momo.jpg', 'Buff Fried Momo', 140, 25, 'These deep-fried buff dumplings are the perfect choice for those who love crispy, fried food. The savory and spicy buff filling is encased in a crispy, golden-brown exterior, making these dumplings a delicious and satisfying snack.', 0, 'bfm', 'Deep-fried buff dumplings', 'flour and water\r\nBuff keema\r\nveggies (such as cabbage, carrots, and onions)', 0, 1, 114),
+(39, 'buff fried momo.jpg', 'Buff Fried Momo', 140, 25, 'These deep-fried buff dumplings are the perfect choice for those who love crispy, fried food. The savory and spicy buff filling is encased in a crispy, golden-brown exterior, making these dumplings a delicious and satisfying snack.', 0, 'bfm', 'Deep-fried buff dumplings', 'flour and water\r\nBuff keema\r\nveggies (such as cabbage, carrots, and onions)                                                        ', 0, 1, 114),
 (40, 'crunchy chicken burger.jpg', 'Crunchy Chicken Burger', 180, 25, 'Sink your teeth into our delicious crunchy fried chicken burger! Made with a crispy fried chicken patty that\'s made from juicy chicken breast coated in a flavorful blend of flour, eggs, bread crumbs, and spices. Served on a soft bun and topped with fresh lettuce, juicy tomato slices, crunchy onion, and tangy pickles, this burger is a feast for your taste buds. Perfect for a quick lunch or dinner, our crunchy fried chicken burger is sure to satisfy your cravings!', 0, 'ccb', 'Crispy fried chicken on a bun.', 'fried chicken patty (made with chicken breast, flour, eggs, bread crumbs, and spices)\r\nlettuce\r\ntomato\r\nonion\r\npickles\r\nbun', 0, 1, 115),
 (41, 'Margherita.jpg', 'Margherita Pizza', 340, 20, ' A classic Italian pizza with tangy tomato sauce, melty slices of creamy mozzarella cheese, and fragrant basil leaves.', 1, 'mp', 'Classic pizza with tomato, mozzarella, and basil', 'tomato sauce\r\nmozzarella cheese\r\nbasil leaves                            ', 0, 0, 120),
 (42, 'pepperoni.jpg', 'Pepperoni Pizza', 350, 25, 'A pizza topped with tangy tomato sauce, melty mozzarella cheese, and slices of spicy pepperoni sausage.', 0, 'pp', 'Pizza with tomato, mozzarella, and pepperoni.', 'tomato sauce \r\nmozzarella cheese \r\npepperoni sausage                                             ', 0, 0, 120),
@@ -264,6 +276,23 @@ CREATE TABLE `kos` (
   `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `kos`
+--
+
+INSERT INTO `kos` (`kos_id`, `order_id`, `status`, `date`) VALUES
+(120, 330, 'rejected', '2023-04-26 09:16:29'),
+(125, 331, 'prepared', '2023-04-26 10:44:22'),
+(126, 332, 'prepared', '2023-04-26 10:44:22'),
+(127, 333, 'prepared', '2023-04-26 10:44:22'),
+(128, 334, 'prepared', '2023-04-26 10:44:22'),
+(132, 327, 'rejected', '2023-04-26 11:44:22'),
+(133, 328, 'rejected', '2023-04-26 11:44:22'),
+(134, 329, 'rejected', '2023-04-26 11:44:22'),
+(147, 335, 'prepared', '2023-04-26 12:00:49'),
+(148, 336, 'prepared', '2023-04-26 12:00:49'),
+(149, 337, 'prepared', '2023-04-26 12:00:49');
+
 -- --------------------------------------------------------
 
 --
@@ -276,13 +305,6 @@ CREATE TABLE `newsletter` (
   `email` varchar(50) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `newsletter`
---
-
-INSERT INTO `newsletter` (`nl_id`, `c_id`, `email`, `date`) VALUES
-(2, 42, 'sd807141@gmail.com', '2023-03-20 13:24:36');
 
 -- --------------------------------------------------------
 
@@ -310,12 +332,22 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `c_id`, `track_id`, `o_c_id`, `qty`, `f_id`, `total_price`, `note`, `payment_method`, `delivery_date`, `delivery_time`, `date`) VALUES
-(316, 42, 'rh00000002', 314, 1, 44, 452, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 12:35:31'),
-(317, 42, 'rh00000003', 315, 1, 39, 158, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 12:48:03'),
-(318, 42, 'rh00000004', 316, 1, 42, 396, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 12:51:42'),
-(319, 42, 'rh00000005', 317, 1, 41, 384, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 13:11:02'),
-(320, 42, 'rh00000005', 317, 1, 43, 452, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 13:11:02'),
-(321, 42, 'rh00000006', 318, 1, 33, 158, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-04 16:15:04');
+(322, 47, 'rh00000007', 319, 1, 44, 452, 'No note', 'COD', '2023-04-08', '10:30:00', '2023-04-07 20:40:39'),
+(323, 47, 'rh00000008', 320, 1, 44, 452, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-08 12:44:55'),
+(324, 48, 'rh00000009', 321, 1, 44, 452, 'No note', 'COD', '2023-04-10', '09:30:00', '2023-04-10 09:28:54'),
+(325, 48, 'rh00000010', 322, 1, 40, 203, 'No note', 'COD', '2023-04-10', '10:21:00', '2023-04-10 09:51:58'),
+(326, 48, 'rh00000011', 323, 1, 38, 181, 'No note', 'COD', '2023-04-10', '18:27:00', '2023-04-10 17:27:59'),
+(327, 48, 'rh00000012', 324, 1, 39, 158, 'No note', 'COD', '2023-04-10', '18:37:00', '2023-04-10 17:38:08'),
+(328, 48, 'rh00000012', 324, 1, 40, 203, 'No note', 'COD', '2023-04-10', '18:37:00', '2023-04-10 17:38:08'),
+(329, 48, 'rh00000012', 324, 1, 44, 452, 'No note', 'COD', '2023-04-10', '18:37:00', '2023-04-10 17:38:08'),
+(330, 47, 'rh00000013', 325, 2, 43, 904, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-24 17:46:35'),
+(331, 47, 'rh00000014', 326, 1, 38, 181, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 09:49:42'),
+(332, 47, 'rh00000014', 326, 2, 36, 271, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 09:49:42'),
+(333, 47, 'rh00000014', 326, 1, 40, 203, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 09:49:42'),
+(334, 47, 'rh00000014', 326, 1, 43, 452, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 09:49:42'),
+(335, 47, 'rh00000015', 327, 1, 39, 158, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 11:47:58'),
+(336, 47, 'rh00000015', 327, 1, 35, 113, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 11:47:58'),
+(337, 47, 'rh00000015', 327, 1, 34, 181, 'No note', 'COD', '0000-00-00', '00:00:00', '2023-04-26 11:47:58');
 
 -- --------------------------------------------------------
 
@@ -341,7 +373,16 @@ INSERT INTO `order_contact_details` (`o_c_id`, `address`, `phone`, `c_name`) VAL
 (315, 'dsf lksdjflk jsdlkfjslkd', '9854546545', 'Suresh Dahal'),
 (316, 'jfksdj lfsjdlfksjd', '9815314535', 'Sunil Sharma'),
 (317, 'ljdsflksjd lkjfslkdjflk', '023454454', 'sdjflskdjlfk'),
-(318, 'dsjkfjs ldkfjslkdf', '9800000000', 'skdjf lksdj');
+(318, 'dsjkfjs ldkfjslkdf', '9800000000', 'skdjf lksdj'),
+(319, 'jf sadlfkdsjfk', '9800000000', 'suresh dahal'),
+(320, 'sjfdskjf lksjdfk', '9800000000', 'sfdfdsf sd '),
+(321, 'sdjflsjdkflsjdf', '9800000000', 'sfdjsdlj lk'),
+(322, 'jfdsklf jlskd', '9800000000', 'sdfksjdl kfj'),
+(323, 'Chardobato, Banepa', '9814565656', 'Jessica Thapa'),
+(324, 'dfjsadklfj asjd lfjldfj l', '9800000000', 'Suresh Dahal'),
+(325, 'fjsla jlsjd flkjal', '9800000000', 'suresh dahal'),
+(326, 'chardobato, banepa', '9800000000', 'Suresh Dahal'),
+(327, 'skldfjjsdflk jdsf', '9800000000', 'jsdk fjlsk');
 
 -- --------------------------------------------------------
 
@@ -351,10 +392,19 @@ INSERT INTO `order_contact_details` (`o_c_id`, `address`, `phone`, `c_name`) VAL
 
 CREATE TABLE `reject_reason` (
   `o_r_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
+  `track_id` varchar(11) NOT NULL,
   `rejected_by` varchar(10) NOT NULL,
   `reason` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reject_reason`
+--
+
+INSERT INTO `reject_reason` (`o_r_id`, `track_id`, `rejected_by`, `reason`) VALUES
+(51, 'rh00000014', 'admin', 'just for fun'),
+(53, 'rh00000013', 'kitchen', 'for kitchen fun'),
+(54, 'rh00000012', 'kitchen', 'just for testing');
 
 -- --------------------------------------------------------
 
@@ -374,12 +424,9 @@ CREATE TABLE `to_be_delivered` (
 --
 
 INSERT INTO `to_be_delivered` (`tbd_id`, `order_id`, `status`, `date`) VALUES
-(58, 316, 'delivered', '2023-04-04 12:36:16'),
-(59, 317, 'delivered', '2023-04-04 12:48:48'),
-(60, 318, 'delivered', '2023-04-04 12:52:05'),
-(61, 319, 'delivered', '2023-04-04 13:11:34'),
-(62, 320, 'delivered', '2023-04-04 13:11:34'),
-(63, 321, 'delivered', '2023-04-04 16:15:36');
+(66, 335, 'delivered', '2023-04-26 12:03:20'),
+(67, 336, 'delivered', '2023-04-26 12:03:20'),
+(68, 337, 'delivered', '2023-04-26 12:03:20');
 
 --
 -- Indexes for dumped tables
@@ -478,8 +525,7 @@ ALTER TABLE `order_contact_details`
 -- Indexes for table `reject_reason`
 --
 ALTER TABLE `reject_reason`
-  ADD PRIMARY KEY (`o_r_id`),
-  ADD KEY `order_id` (`order_id`);
+  ADD PRIMARY KEY (`o_r_id`);
 
 --
 -- Indexes for table `to_be_delivered`
@@ -502,13 +548,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `aos`
 --
 ALTER TABLE `aos`
-  MODIFY `aos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=382;
+  MODIFY `aos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=398;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=251;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=266;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -520,7 +566,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `department`
@@ -544,13 +590,13 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT for table `future_orders`
 --
 ALTER TABLE `future_orders`
-  MODIFY `fo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `fo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `kos`
 --
 ALTER TABLE `kos`
-  MODIFY `kos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `kos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
 
 --
 -- AUTO_INCREMENT for table `newsletter`
@@ -562,25 +608,25 @@ ALTER TABLE `newsletter`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=322;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=338;
 
 --
 -- AUTO_INCREMENT for table `order_contact_details`
 --
 ALTER TABLE `order_contact_details`
-  MODIFY `o_c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=319;
+  MODIFY `o_c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=328;
 
 --
 -- AUTO_INCREMENT for table `reject_reason`
 --
 ALTER TABLE `reject_reason`
-  MODIFY `o_r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `o_r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `to_be_delivered`
 --
 ALTER TABLE `to_be_delivered`
-  MODIFY `tbd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `tbd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- Constraints for dumped tables
@@ -636,12 +682,6 @@ ALTER TABLE `orders`
   ADD CONSTRAINT `c_id` FOREIGN KEY (`c_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `f_id` FOREIGN KEY (`f_id`) REFERENCES `food` (`f_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `o_c_id` FOREIGN KEY (`o_c_id`) REFERENCES `order_contact_details` (`o_c_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `reject_reason`
---
-ALTER TABLE `reject_reason`
-  ADD CONSTRAINT `reject_reason_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
 -- Constraints for table `to_be_delivered`
